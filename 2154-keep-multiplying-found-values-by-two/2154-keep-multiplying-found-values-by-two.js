01973afeaaf379023cf5ce39f2670469d4c979ad
Yididiya G. <yidextech@gmail.com>
2025-11-19 23:47:24 +0300

    Time: 0 ms (100%), Space: 55.6 MB (36.61%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+    nums = new Set(nums)
+
+    while(nums.has(original)){
+        original = 2*original
+    }
+    return original
+};
