205ba03e30a9d2e3685a351947cee726a0bb909a
Yididiya G. <yidextech@gmail.com>
2025-12-06 21:26:56 +0300

    Time: 0 ms (100%), Space: 17.7 MB (89.9%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -9,15 +9,12 @@ class Solution:
             "D":500,
             "M":1000
         }
-        res = 0
-        i = 0
-        while i < len(s)-1:
-            if values[s[i]] >= values[s[i+1]]:
-                res += values[s[i]]
-                i += 1
-                if i == len(s)-1:
-                    return res + values[s[i]]
+        res = values[s[0]]
+
+        for i in range(1, len(s)):
+            if values[s[i]] > values[s[i-1]]:
+                res -= values[s[i-1]]
+                res += values[s[i]] - values[s[i-1]]
             else:
-                res += values[s[i+1]] - values[s[i]]
-                i += 2
+                res += values[s[i]]
         return res
