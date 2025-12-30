f7c3179685356eda13e135319b050b2137aaa924
Yididiya G. <yidextech@gmail.com>
2025-12-30 21:00:29 +0300

    Time: 5 ms (65.58%), Space: 19.6 MB (17.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
-        hashTable = {}
-        for num in nums:
-            if num in hashTable:
-                hashTable[num] += 1
-            else:
-                hashTable[num] = 1
-            if hashTable[num] > len(nums)//2:
-                    return num
+        nums_dict= Counter(nums)
+
+        for num, c in nums_dict.items():
+            if c > (len(nums)//2):
+                return num
