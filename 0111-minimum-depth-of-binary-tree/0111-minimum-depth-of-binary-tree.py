d8fceda3589e80a9aa1d9f64a41b93e7d193e2db
Yididiya G. <yidextech@gmail.com>
2026-01-05 23:20:58 +0300

    Time: 0 ms (100%), Space: 43.7 MB (82.11%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -12,15 +12,13 @@ class Solution:
         dq = deque()
         dq.append(root)
         h = 1
-        min_h = float("inf")
         while dq:
             for i in range(len(dq)):
                 cur = dq.popleft()
                 if not(cur.left or cur.right):
-                    min_h = min(min_h, h)
+                    return h
                 if cur.left:
                     dq.append(cur.left)
                 if cur.right:
                     dq.append(cur.right)
             h += 1
-        return min_h
