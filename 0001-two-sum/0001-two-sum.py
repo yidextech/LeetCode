dc4655c6d089e1c46aaf5452e3e9e4cf80f8518e
Yididiya G. <yidextech@gmail.com>
2026-02-07 12:56:45 +0300

    Time: 4 ms (40.73%), Space: 20.5 MB (24.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        old_nums = nums[:]
-        nums.sort()
+        
+        check = defaultdict(int)
 
-        l = 0
-        r = len(nums)-1
+        for i in range(len(nums)):
+            diff = target-nums[i]
+            if diff in check:
+                return [check[diff],i]
+            check[nums[i]] = i
 
-        while l<r:
-            if nums[l] + nums[r] == target:
-                i = old_nums.index(nums[l])
-                j = old_nums.index(nums[r])
-                if i <=j:
-                    old_nums.pop(i)
-                    j = old_nums.index(nums[r])+1
-                return [i,j]
-            elif nums[l] + nums[r] < target:
-                l += 1
-            else:
-                r -= 1
+        
