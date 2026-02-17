39ad0e917c639ffed59f1ec5bb554360d78ed244
Yididiya G. <yidextech@gmail.com>
2026-02-17 11:34:21 +0300

    Time: 1591 ms (5.07%), Space: 21.7 MB (41.51%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,21 +1,24 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         
-        groups = defaultdict(list)
+        row = len(mat)
+        col = len(mat[0])
+        n = row+col-2
 
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                key = i+j
-                groups[key].append(mat[i][j])
-    
-        print(groups)
-        turn = 0
-        output =[]
-        for key, value in groups.items():
-            if not turn:
-                output.extend(value[::-1])
+        res = []
+        for i in range(n+1):
+            #odd
+            if i%2 != 0:
+                for j in range(min(i+1,row)):
+                    indx = i-j
+                    if indx < col:
+                        res.append(mat[j][indx])
+            #even
             else:
-    
-                output.extend(value)
-            turn = 1-turn
-        return output
+                for j in range(min(i,row-1), -1, -1):
+                    indx = i-j
+                    if indx < col:
+                        res.append(mat[j][indx])
+
+        return res
+
