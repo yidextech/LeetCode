7160bf342fe7121a78a27858e52a7c312a49b526
Yididiya G. <yidextech@gmail.com>
2026-03-16 20:33:39 +0300

    Time: 1729 ms (6.48%), Space: 33.3 MB (6.97%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,8 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k %= n
-        nums.reverse()
-        nums[:k] = reversed(nums[:k])
-        nums[k:] = reversed(nums[k:])
+        
+        def solution(k, arr):
+            if k == 0:
+                return 
+            k -= 1
+            front = arr.pop()
+            arr.insert(0, front)
+            solution(k, arr)
+        
+        solution(k, nums)
